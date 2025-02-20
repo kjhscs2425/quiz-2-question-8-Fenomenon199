99944ceb0509956594e62ec0956178e0aa79cf51
Fenomenon199 <afeldman2025@kehillahstudent.org>
2025-02-20 11:36:43 -0800

    quiz 2

--- q8.py
+++ q8.py
@@ -6,5 +6,22 @@ ends with one of these letters: p, t, n
 
 (This should print out 18 words in total)
 """
+# print: ("c","t","b")
+# words = ("cat", "boat", "bat", "pat", "nat", "nap", "tap", "cap","tan", "top", "bot", "pot", "not", "coat", 
 
-# YOUR CODE HERE
+c_t_b = 'ctb'
+for letter1 in c_t_b:
+    a_o= 'ao' 
+    for letter2 in a_o: 
+        # print(letter1)
+        # print (letter2 )
+        print (letter1+letter2)
+
+# a_string ='a string' 
+# for letter in a_string:
+#     print : ("t") 
+
+# a_string ='a string' 
+# for letter in a_string:
+#     print:  ("b" )
+  
